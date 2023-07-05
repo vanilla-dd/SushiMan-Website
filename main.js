43673b843e01df74ad68cffd07c8de98cb6c3fc4
whiteHawk <freeidlll6@gmail.com>
2023-07-05 23:51:29 +0530

    🫡 added nav layout for mobile and completed nav styling for big screens

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-import "./styles/style.css";
+import "./styles/reset.css";
+import "./styles/utils.css";
+import "./styles/header.css";
+const hamburger = document.querySelector(".open");
+const nav = document.querySelector(".nav__links");
+hamburger.addEventListener("click", (e) => {
+  nav.classList.toggle("show");
+});
