9f0ce76f477fef51b32d9b14f4bf4544dc04298a
whiteHawk <freeidlll6@gmail.com>
2023-07-07 00:09:36 +0530

    🫡 first section complete(not reponsive rn)

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+// css
 import "./styles/reset.css";
 import "./styles/utils.css";
 import "./styles/header.css";
+import "./styles/sec-1.css";
+// javascript
 import navigation from "./components/header";
 navigation();
