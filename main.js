2d4e95c511bf2535fd6e5ed39ce2fcb080274abc
whiteHawk <freeidlll6@gmail.com>
2023-07-08 14:26:53 +0530

    🥳 section 2 is completed and is fully responsive + some bug fixes

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./styles/reset.css";
 import "./styles/utils.css";
 import "./styles/header.css";
 import "./styles/sec-1.css";
+import "./styles/sec-2.css";
 // javascript
 import navigation from "./components/header";
 navigation();
