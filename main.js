2ead3ba6da39ca69ab5b787b3c2691eca41f14a8
whiteHawk <freeidlll6@gmail.com>
2023-07-09 19:54:04 +0530

    ✅ footer done + responsive

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ import "./styles/sec-2.css";
 import "./styles/sec-3.css";
 import "./styles/sec-4.css";
 import "./styles/sec-5.css";
+import "./styles/footer.css";
 // javascript
 import navigation from "./components/header";
 navigation();
