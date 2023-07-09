6f854520b8142a714586c220ef30314e855c747a
whiteHawk <freeidlll6@gmail.com>
2023-07-09 19:16:40 +0530

    ✅ section 5 done + responsive

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import "./styles/sec-1.css";
 import "./styles/sec-2.css";
 import "./styles/sec-3.css";
 import "./styles/sec-4.css";
+import "./styles/sec-5.css";
 // javascript
 import navigation from "./components/header";
 navigation();
